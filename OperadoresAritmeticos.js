6e366c951616e835e46b4633d603c03607d75f8f
Emiirck <emirck.candelo@tecnologicoliceocristiano.edu.ec>
2025-11-28 21:28:00 -0500

    OperadoresAritmeticos.js

---
+++ OperadoresAritmeticos.js
@@ -0,0 +1,12 @@
+console.log(`La suma es ${suma}`)
+
+let resta = numerouno - numerodos;
+console.log(`La suma es ${resta}`)
+
+let multi = numerouno * numerodos;
+console.log(`La suma es ${multi}`)
+
+let divi = numerouno / numerodos;
+console.log(`La suma es ${divi}`)
+
+console.log(`La suma es ${equal}`)
